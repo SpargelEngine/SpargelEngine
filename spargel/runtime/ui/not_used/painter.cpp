56f580a96bbd63df5be1a81fc6862e040370aedb
Tianjiao Nie <ivwumupy@gmail.com>
2025-10-24 23:20:54 +0800

    remove compute-based path

--- spargel/runtime/ui/not_used/painter.cpp
+++
@@ -1,5 +0,0 @@
-#include "spargel/runtime/ui/painter.h"
-
-namespace spargel::runtime::ui {
-void CommandList::add_triangle(Vec2f p1, Vec2f p2, Vec2f p3, uint32_t color) {}
-}  // namespace spargel::runtime::ui
